7e3192a9b74ec178515b5f8f9d74e6386c43cfe0
unknown <jadisvara@gmail.com>
2016-11-23 17:43:42 +0300

    first push

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App cat={3} />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    inline: true,
+    port: 3333
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+}

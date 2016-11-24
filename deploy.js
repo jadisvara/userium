7fc41b979ee56261a4679f88e74139cd2908eff8
unknown <jadisvara@gmail.com>
2016-11-24 15:53:20 +0300

    trying to chatch an error

--- deploy.js
+++ deploy.js
@@ -5,9 +5,10 @@ if (process.env.NODE_ENV === 'production') {
   // We basically just create a child process that will run
   // the production bundle command
   var child_process = require('child_process');
-  child_process.exec("webpack -p --config webpack.prod.config.js", function (error, stdout, stderr) {
+  child_process.exec("webpack -p --config webpack.prod.config.js --display-error-details --debug", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
+    console.log('error: ' + error);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
